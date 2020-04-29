437b4f5a6bb28c263db7c6738b42a929d520ecc7
liu5070710 <740844539@qq.com>
2020-04-30 01:52:49 +0800

    完成toast默认样式设置

--- src/toast.vue
+++ src/toast.vue
@@ -9,5 +9,17 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+.toast {
+   position: fixed;
+   display:flex;
+   align-items: center;
+   transform: translateX(-50%);
+   left: 50%;
+   padding: 0 15px;
+   height: 50px;
+   border-radius: 4px;
+   font-size: 0.8em;
+   background-color: rgba($color: #000000, $alpha: .75);
+   color: #fff;
+}
 </style>
